e266893c669ceb388a34e86d602916b6403cee42
fitsum belayneh <fitsu_alt@yahoo.com>
2018-01-16 21:29:58 -0500

    login route

--- server/auth/index.js
+++ server/auth/index.js
@@ -22,3 +22,23 @@ router.get('/user', (req, res, next) => {
 		return res.json({ user: null })
 	}
 })
+
+router.post(
+	'/login',
+	function(req, res, next) {
+		console.log(req.body)
+		console.log('================')
+		next()
+	},
+	passport.authenticate('local'),
+	(req, res) => {
+		console.log('POST to /login')
+		const user = JSON.parse(JSON.stringify(req.user)) // hack
+		const cleanUser = Object.assign({}, user)
+		if (cleanUser.local) {
+			console.log(`Deleting ${cleanUser.local.password}`)
+			delete cleanUser.local.password
+		}
+		res.json({ user: cleanUser })
+	}
+)
