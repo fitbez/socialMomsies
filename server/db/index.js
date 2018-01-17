c3444e5b9738d8029214a0ac556cb11b18c5ff21
fitsum belayneh <fitsu_alt@yahoo.com>
2018-01-16 21:42:30 -0500

    google strategy

--- server/db/index.js
+++ server/db/index.js
@@ -13,3 +13,15 @@ if (process.env.MONGODB_URI) {
 	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
 	MONGO_URL = MONGO_LOCAL_URL
 }
+
+const db = mongoose.connection
+db.on('error', err => {
+	console.log(`There was an error connecting to the database: ${err}`)
+})
+db.once('open', () => {
+	console.log(
+		`You have successfully connected to your mongo database: ${MONGO_URL}`
+	)
+})
+
+module.exports = db
