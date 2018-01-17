a10c84ab253aedfe7ce2faf41303530d04076f63
fitsum belayneh <fitsu_alt@yahoo.com>
2018-01-16 21:02:32 -0500

    first commit

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,50 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-const Navbar = props =>
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <Link className="navbar-brand" to="/">
-        SocialMomsies
-        </Link>
-      </div>
-      <ul className="nav navbar-nav ">
-      <li
-        className={
-          window.location.pathname === "/" ||
-          window.location.pathname === "/about"
-            ? "active"
-            : ""
+const Nav = ({ user }) => {
+  return(
+    <nav className="navbar">
+      <div className="logo">LOGO</div>
+      <div>
+        {
+          user && user._id ? <Link to="logout">Log Out</Link> : <Link to="login">Log In</Link>
         }
-      >
-        <Link to="/">About</Link>
-      </li>
+      </div>
+    </nav>
+  )
+}
 
-      </ul>
-      <ul className="nav navbar-nav navbar-right">
-
-        <li className={window.location.pathname === "/search" ? "active" : ""}>
-          <Link to="/search">Explore</Link>
-        </li>
-        <li className={window.location.pathname === "/playgroup" ? "active" : ""}>
-          <Link to="/playgroup">Playgroups</Link>
-        </li>
-
-        <li className={window.location.pathname === "/login" ? "active" : ""}>
-          <Link to="/login">Login</Link>
-        </li>
-        <li
-          className={
-            window.location.pathname === "/signup" ? "active" : ""
-            }
-            >
-          <Link to="/signup">SignUp</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>;
-
-export default Navbar;
+export default Nav;
