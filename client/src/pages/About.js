573f616f2e3a813fa5faaadfab3f01b7d4c51fec
Esperanza <el.freeman1@yahoo.com>
2018-01-20 08:14:53 -0500

    test heroku

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,17 +2,11 @@ import React from "react";
 import googleButton from './Login/google_signin_buttons/web/1x/btn_google_signin_light_normal_web.png'
 import './about.css';
 const About = (props) =>
-<<<<<<< HEAD
   <div className="margin">
 
 		<h1>Social Momsies</h1>
 		<h2>Where all Moms are Welcome!</h2>
 
-=======
-  <div className='About'>
-		<h1>SocialMomsies</h1>
-		<h2>Where all MOMs are Welcome!</h2>
->>>>>>> d5ab9048d41dfb5cfce83f96448126e741fe3f9c
 		<a href="http://localhost:3001/auth/google/">
 			{/*<GoogleButton /> */}
 			<img src={googleButton} alt="sign into Google Button" />
