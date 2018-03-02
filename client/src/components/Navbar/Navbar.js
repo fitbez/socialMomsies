b57b0bf3242f5c9950573d574f50fc9a89f33062
fitsum belayneh <fitsu_alt@yahoo.com>
2018-03-02 13:00:05 -0500

    fix navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -23,37 +23,35 @@ class Navbar extends Component {
 
 	render() {
 		return (
-			<nav className="navbar navbar-default navbar-fixed-top"  ref={(element) => this.navbarElement = element}>
-				<div className='container-fluid'>
-					<div className="navbar-header">
-						<Link className="navbar-brand" to="/">
-							<img alt='Social Momsies' src={logo} style={{width:100, height: 60, marginTop: -17}} />
-						</Link>
-					</div>
-					{
-						(this.props.user && this.props.user._id) &&
-						<ul className="nav navbar-nav navbar-right">
-							<li className={window.location.pathname === "/search" ? "active" : ""}>
-								<Link to="/search">Explore</Link>
-							</li>
-							<li className={window.location.pathname === "/playgroup" ? "active" : ""}>
-								<Link to="/playgroup">Playgroups</Link>
-							</li>
-							<li>
-								<a href="/" className="logout" onClick={this.props.logout}>Logout</a>
-							</li>
-						</ul>
-					}
-					{
-						(!this.props.user) &&
-						<ul className="nav navbar-nav navbar-right">
-							<a href={window.location.hostname === 'localhost' ? "http://localhost:3001/auth/google/" :  "https://socialmomsies.herokuapp.com/auth/google/"}>
-								{/*<GoogleButton /> */}
-								<img src={googleButton} alt="sign into Google Button" />
-							</a>
-						</ul>
-					}
+			<nav className="menubar"  ref={(element) => this.navbarElement = element}>
+				<div>
+					<Link to="/">
+						<img alt='Social Momsies' src={logo} style={{width:100, height: 60}} />
+					</Link>
 				</div>
+				{
+					(this.props.user && this.props.user._id) &&
+					<ul className="nav navbar-nav navbar-right inner-nav">
+						<li className={window.location.pathname === "/search" ? "active" : ""}>
+							<Link to="/search">Explore</Link>
+						</li>
+						<li className={window.location.pathname === "/playgroup" ? "active" : ""}>
+							<Link to="/playgroup">Playgroups</Link>
+						</li>
+						<li>
+							<a href="/" className="logout" onClick={this.props.logout}>Logout</a>
+						</li>
+					</ul>
+				}
+				{
+					(!this.props.user) &&
+					<ul className="nav navbar-nav navbar-right inner-nav">
+						<a href={window.location.hostname === 'localhost' ? "http://localhost:3001/auth/google/" :  "https://socialmomsies.herokuapp.com/auth/google/"}>
+							{/*<GoogleButton />*/}
+							<img src={googleButton} alt="sign into Google Button" />
+						</a>
+					</ul>
+				}
 			</nav>
 		);
 	}
