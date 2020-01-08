302f723b86d0d42c9b798a047c5678ea385bc754
fitsum belayneh <fitsu_alt@yahoo.com>
2020-01-08 17:35:11 -0500

    home page style updated

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import yelp from './yelp.png';
-import './SearchBar.css';
-import { Row, Col} from 'react-bootstrap';
+import React from "react";
+import "./SearchBar.css";
+import { Row, Col } from "react-bootstrap";
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',
-      location: '',
-      sortBy: 'best_match'
+      term: "",
+      location: "",
+      sortBy: "best_match"
     };
 
     this.handleSortByChange = this.handleSortByChange.bind(this);
@@ -19,18 +18,16 @@ class SearchBar extends React.Component {
   }
 
   sortByOptions = {
-
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
   };
 
   getSortByClass(sortByOption) {
     if (this.state.sortBy === sortByOption) {
-      return 'active';
+      return "active";
     } else {
-      return '';
+      return "";
     }
   }
 
@@ -60,63 +57,71 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-    return <li className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}  key={sortByOptionValue}>{sortByOption}</li>;
+      return (
+        <li
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+          key={sortByOptionValue}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   }
 
-    render() {
-      return(
-
+  render() {
+    return (
       <div className="SearchBar">
-
         <Row>
           <Col md={4}>
+            <div className="SearchBar-fields">
+              <label>
+                <select
+                  className="Search-term"
+                  term={this.state.value}
+                  onChange={this.handleTermChange}
+                >
+                  <option value="">Select Activity </option>
+                  <option value="Playgrounds">Playgrounds</option>
+                  <option value="Splash Parks">Splash Parks</option>
+                  <option value="Kid-Friendly Restaurants">Restaurants</option>
+                  <option value="Libraries">Libraries</option>
+                  <option value="Museums">Museums</option>
+                  <option value="Kid-Activities">Kid Events</option>
+                  <option value="Schools">Schools</option>
+                  <option value="Hospitals">Hospitals</option>
+                </select>
+              </label>
+              {/*<input className="Search-term" placeholder="City or Zip Code"  term={this.state.value} onChange={this.handleLocationChange} />*/}
 
-              <div className="SearchBar-fields">
-                <label>
-               <select className="Search-term" term={this.state.value} onChange={this.handleTermChange}>
-                <option value="">Select Activity </option>
-                 <option value="Playgrounds">Playgrounds</option>
-                 <option value="Splash Parks">Splash Parks</option>
-                 <option value="Kid-Friendly Restaurants">Restaurants</option>
-                 <option value="Libraries">Libraries</option>
-                 <option value="Museums">Museums</option>
-                 <option value="Kid-Activities">Kid Events</option>
-                 <option value="Schools">Schools</option>
-                 <option value="Hospitals">Hospitals</option>
-               </select>
-             </label>
-             {/*<input className="Search-term" placeholder="City or Zip Code"  term={this.state.value} onChange={this.handleLocationChange} />*/}
-
-            <label>
-             <select  className="Search-fields" term={this.state.location} onChange={this.handleLocationChange}>
-              <option value="">Select city</option>
-               <option value="Arlington">Arlington, Va</option>
-               <option value="20011">Washington, DC</option>
-               <option value="New York">New York</option>
-               <option value="Los Angeles">Los Angeles, Ca</option>
-             </select>
-            </label>
-              </div>
-              <div className="SearchBar-sort-options">
-                <ul>
-                  {this.renderSortByOptions()}
-                </ul>
-              </div>
-              <div className="SearchBar-submit">
-                <a onClick={this.handleSearch}> Search powered by
-                 <img src={yelp} style={{width:70,height:55}} alt="Yelp" /></a>
-              </div>
-        </Col>
-        <Col md={8}>
-                  <div>
-
-                  </div>
-        </Col>
-  </Row>
-</div>
-      );
-    }
+              <label>
+                <select
+                  className="Search-fields"
+                  term={this.state.location}
+                  onChange={this.handleLocationChange}
+                >
+                  <option value="">Select city</option>
+                  <option value="Arlington">Arlington, Va</option>
+                  <option value="20011">Washington, DC</option>
+                  <option value="New York">New York</option>
+                  <option value="Los Angeles">Los Angeles, Ca</option>
+                </select>
+              </label>
+            </div>
+            <div className="SearchBar-sort-options">
+              <ul>{this.renderSortByOptions()}</ul>
+            </div>
+            <div className="SearchBar-submit">
+              <a onClick={this.handleSearch}> Search</a>
+            </div>
+          </Col>
+          <Col md={8}>
+            <div></div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
