3608973556811261dbde0b741bca272f6549e4d5
fitsum belayneh <fitsu_alt@yahoo.com>
2020-01-09 15:59:02 -0500

    loading ... is added

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,53 +1,72 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router-dom';
-import Yelp from '../util/Yelp.js';
-import BusinessList from '../components/BusinessList/BusinessList.js';
-import SearchBar from '../components/SearchBar/SearchBar.js';
-// import Footer from "./components/Footer";
-import { Row, Col, Alert } from 'react-bootstrap';
-import './Search.css';
+import { Redirect } from "react-router-dom";
+import Yelp from "../util/Yelp.js";
+import BusinessList from "../components/BusinessList/BusinessList.js";
+import SearchBar from "../components/SearchBar/SearchBar.js";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner.js";
+import { Row, Col, Alert } from "react-bootstrap";
+import "./Search.css";
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       businesses: [],
-			alert: null,
+      alert: null
     };
     this.searchYelp = this.searchYelp.bind(this);
   }
   searchYelp(term, location, sortBy) {
-		if (!term || term.trim().length < 1 || !location || location.trim().length < 1) {
-			this.setState({alert: 'Please select an activity and location!'});
+    if (
+      !term ||
+      term.trim().length < 1 ||
+      !location ||
+      location.trim().length < 1
+    ) {
+      this.setState({ alert: "Please select an activity and location!" });
 
-			return;
-		}
+      return;
+    }
+    this.setState({ loading: true });
     Yelp.search(term, location, sortBy).then(businesses => {
       this.setState({
-        businesses: (businesses || []),
-				alert: null,
+        businesses: businesses || [],
+        alert: null,
+        loading: false
       });
     });
   }
 
-
   render() {
-    if(!this.props.user) {
-      return (<Redirect to="/" />)
+    const { loading } = this.state;
+    if (!this.props.user) {
+      return <Redirect to="/" />;
     }
     return (
-      <div className="Search" >
-				{this.state.alert && <Alert style={{top: (this.props.navbarHeight + 10 + 'px')}} id='search-alert' bsStyle="danger">{this.state.alert}</Alert>}
+      <div className="Search">
+        {this.state.alert && (
+          <Alert
+            style={{ top: this.props.navbarHeight + 10 + "px" }}
+            id="search-alert"
+            bsStyle="danger"
+          >
+            {this.state.alert}
+          </Alert>
+        )}
 
-				<Row>
-					<Col sm={4} md={4}>
-						<SearchBar search={this.searchYelp}/>
-					</Col>
-					<Col sm={8} md={8}>
-						<BusinessList businesses={this.state.businesses} />
-					</Col>
-				</Row>
-			</div>
+        <Row>
+          <Col sm={4} md={4}>
+            <SearchBar search={this.searchYelp} />
+          </Col>
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            <Col sm={8} md={8}>
+              <BusinessList businesses={this.state.businesses} />
+            </Col>
+          )}
+        </Row>
+      </div>
     );
   }
 }
